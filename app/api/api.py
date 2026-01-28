56899780183f9c4c70a8310522fadc6a405d83ce
dawwson <shines1427@gmail.com>
2026-01-28 23:39:02 +0900

    fix: import 잘못된 경로 수정

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.domains.auth.router import router as auth_router
 from app.domains.diary.router import router as diary_router
-from app.domains.diary.router import router as feedback_router
+from app.domains.feedback.router import router as feedback_router
 
 api_router = APIRouter()
 
